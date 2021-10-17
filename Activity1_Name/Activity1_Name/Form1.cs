63174f03525b7f979074e312e41f39fb936ce546
cepenicahs <cepenicah@gmail.com>
2021-10-18 04:43:31 +0800

    first commit

---
+++ Activity1_Name/Activity1_Name/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity1_Name
+{
+    public partial class Form1 : Form
+    {
+        string fn = "Nicah";
+        string mn = "Sabay";
+        string ln = "Cepe";
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            MessageBox.Show(fn + " " + mn + " " + ln, "This is my full name");
+        }
+    }
+}
