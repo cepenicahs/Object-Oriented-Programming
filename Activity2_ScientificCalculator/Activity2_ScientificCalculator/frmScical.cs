169ff5e660159635607b7750caeddc0da4c347e2
Nicah S. Cepe <88813289+cepenicahs@users.noreply.github.com>
2021-10-26 18:21:36 +0800

    Activity 2: Scientific Calculator Design

---
+++ Activity2_ScientificCalculator/Activity2_ScientificCalculator/frmScical.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Activity2_ScientificCalculator
+{
+    public partial class frmScical : Form
+    {
+        public frmScical()
+        {
+            InitializeComponent();
+        }
+
+        private void frmScical_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
